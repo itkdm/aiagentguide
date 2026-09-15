dd834a5708a3daa65a9764141c3f521b4e7accc5
Bujidao <3317431882@qq.com>
2026-09-15 22:57:42 +0800

    chore: hide rag from top navigation

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -3,6 +3,5 @@ export const siteNav: DefaultTheme.NavItem[] = [
   { text: '首页', link: '/' },
   { text: '入门', link: '/getting-started/' },
   { text: '原理', link: '/principles/' },
-  { text: '项目实践', link: '/projects/', activeMatch: '/projects/' },
-  { text: 'RAG', link: '/rag/' }
+  { text: '项目实践', link: '/projects/', activeMatch: '/projects/' }
 ]
